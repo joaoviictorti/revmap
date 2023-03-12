a3247b5ba4fcf72a2f16341692496af6403cb1f1
João <joaovictorti08@gmail.com>
2023-03-12 19:58:48 -0300

    revmap 0.0.1

---
+++ revmap.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+from revmap import revmap
+
+if __name__ == "__main__":
+    revmap.argumentos()
---
+++ revmap/__meta__.py
@@ -0,0 +1,8 @@
+__version__ = '0.0.1'
+__version_tuple__ = version_tuple = (0, 0, 1)
+__title__ = "revshell"
+__description__ = "Tool that generates reverse shell in multiple languages and encodes"
+__url__ = "https://github.com/joaoviictorti/Revshell"
+__author__ = "joaoviictorti (viictorjj)"
+__author_email__ = "joaovictorti08@gmail.com"
+__copyright__ = "Copyright João Victor"
