d892d0640bc9c3b2106d6b547d9b5f7e770b27fe
João <joaovictorti08@gmail.com>
2023-03-15 20:40:53 -0300

    Reajuste seguindo a sintaxe do blue check & adicionando o mkdocs

--- revmap.py
+++ revmap.py
@@ -2,5 +2,5 @@
 # -*- coding: UTF-8 -*-
 from revmap import revmap
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     revmap.argumentos()
--- revmap/__main__.py
+++ revmap/__main__.py
@@ -1,4 +1,4 @@
 from . import revmap
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     revmap.argumentos()
